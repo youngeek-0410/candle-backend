a6efdaee97b3623bb3abcc82626c2ca0f3c830e2
fuu38 <contact@fumiya.biz>
2023-12-12 17:00:34 +0900

    add all functions

--- lib/candle-backend-stack.ts
+++ lib/candle-backend-stack.ts
@@ -25,27 +25,78 @@ export class CandleBackendStack extends cdk.Stack {
 
 
     // Resolve requests with Lambda
+    //bundlingの設定を書く必要があった
+    //ref:https://iret.media/79515
+    const goLambdaBundleConfig ={
+      bundling: {
+        image: DockerImage.fromRegistry("golang:1.21"),
+        command: [
+          'bash', '-c', [
+            "export GOCACHE=/tmp/go-cache",
+            "export GOPATH=/tmp/go-path",
+            "CGO_ENABLED=0 GOOS=linux go build -tags lambda.norpc -o /asset-output/bootstrap main.go",
+          ].join(" && "),
+        ],
+      },
+    }
     const room = api.root.addResource('room');
 
+    //room:POST
     const roomPOSTHandler = new lambda.Function(this, 'CandleBackendRoomPOSTHandler', {
       functionName: 'RoomPOSTHandler',
       runtime: lambda.Runtime.PROVIDED_AL2,
       handler: 'bootstrap',
-      code: lambda.Code.fromAsset('lambda/room/POST', {
-        bundling: {
-          image: DockerImage.fromRegistry("golang:1.21"),
-          command: [
-            'bash', '-c', [
-              "export GOCACHE=/tmp/go-cache",
-              "export GOPATH=/tmp/go-path",
-              "CGO_ENABLED=0 GOOS=linux go build -tags lambda.norpc -o /asset-output/bootstrap main.go",
-            ].join(" && "),
-          ],
-        },
-      }),
+      code: lambda.Code.fromAsset('lambda/room/POST',goLambdaBundleConfig),
     });
     roomTable.grantReadWriteData(roomPOSTHandler);
-
     room.addMethod('POST', new apigateway.LambdaIntegration(roomPOSTHandler))
+    
+    //room/{room_id}:POST
+    const roomId = room.addResource('{room_id}');
+    const roomIdPOSTHandler = new lambda.Function(this, 'CandleBackendRoomIdPOSTHandler', {
+      functionName: 'RoomIdPOSTHandler',
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'bootstrap',
+      code: lambda.Code.fromAsset('lambda/room/{room_id}/POST',goLambdaBundleConfig),
+    });
+    roomTable.grantReadWriteData(roomIdPOSTHandler);
+    userTable.grantReadWriteData(roomIdPOSTHandler);
+    roomId.addMethod('POST', new apigateway.LambdaIntegration(roomIdPOSTHandler))
+
+    //room/{room_id}/start:GET
+    const start = roomId.addResource('start');
+    const roomIdStartGETHandler = new lambda.Function(this, 'CandleBackendRoomIdStartGETHandler', {
+      functionName: 'RoomIdStartGETHandler',
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'bootstrap',
+      code: lambda.Code.fromAsset('lambda/room/{room_id}/start/GET',goLambdaBundleConfig),
+    });
+    roomTable.grantReadWriteData(roomIdStartGETHandler);
+    userTable.grantReadWriteData(roomIdStartGETHandler);
+    start.addMethod('GET', new apigateway.LambdaIntegration(roomIdStartGETHandler))
+    
+    //room/{room_id}/result:GET
+    const result = roomId.addResource('result');
+    const roomIdResultGETHandler = new lambda.Function(this, 'CandleBackendRoomIdResultGETHandler', {
+      functionName: 'RoomIdResultGETHandler',
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'bootstrap',
+      code: lambda.Code.fromAsset('lambda/room/{room_id}/result/GET',goLambdaBundleConfig),
+    });
+    roomTable.grantReadWriteData(roomIdResultGETHandler);
+    userTable.grantReadWriteData(roomIdResultGETHandler);
+    result.addMethod('GET', new apigateway.LambdaIntegration(roomIdResultGETHandler))
+
+    //room/{room_id}/result:POST
+    const roomIdResultPOSTHandler = new lambda.Function(this, 'CandleBackendRoomIdResultPOSTHandler', {
+      functionName: 'RoomIdResultPOSTHandler',
+      runtime: lambda.Runtime.PROVIDED_AL2,
+      handler: 'bootstrap',
+      code: lambda.Code.fromAsset('lambda/room/{room_id}/result/POST',goLambdaBundleConfig),
+    });
+    roomTable.grantReadWriteData(roomIdResultPOSTHandler);
+    userTable.grantReadWriteData(roomIdResultPOSTHandler);
+    result.addMethod('POST', new apigateway.LambdaIntegration(roomIdResultPOSTHandler))
+
   }
 }
