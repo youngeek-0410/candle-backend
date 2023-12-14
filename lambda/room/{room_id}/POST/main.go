c1ab8e3a155e4043fe3f328f46043c5aaba0b513
GoRuGoo <dachanyr8989@gmail.com>
2023-12-14 14:16:50 +0900

    wip: room enterエンドポイントを実装(テストまだ)

--- lambda/room/{room_id}/POST/main.go
+++ lambda/room/{room_id}/POST/main.go
@@ -2,24 +2,110 @@ package main
 
 import (
 	"context"
-	"fmt"
-
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/google/uuid"
+	"net/http"
+	"os"
 )
 
-type Response struct {
-	Body string `json:"body"`
+type Answer struct {
+	QuestionId string `json:"question_id"`
+	Answer     bool   `json:"answer"`
+}
+type UserData struct {
+	UserId   string   `json:"uid"`
+	NickName string   `json:"nickname"`
+	Answers  []Answer `json:"answers"`
 }
 
-func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("room/{%s}/%s\n", event.PathParameters["room_id"], event.HTTPMethod)
+type requestBody struct {
+	NickName string   `json:"nickname"`
+	Answers  []Answer `json:"answers"`
+}
+
+func enterRoomHandler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	roomId := event.PathParameters["room_id"]
+	if roomId == "" {
+		return createEmptyResponseWithStatus(400, "Incorrect path parameter")
+	}
+
+	var req requestBody
+	if err := json.Unmarshal([]byte(event.Body), &req); err != nil {
+		return createEmptyResponseWithStatus(500, "JSON parse error")
+	}
+
+	//リクエストボディにuser_idは含まれていないので新しい構造体を使ってデータ挿入
+	var userData UserData
+	userId := uuid.New()
+
+	userData.UserId = userId.String()
+	userData.NickName = req.NickName
+	userData.Answers = req.Answers
+
+	// 書き込み処理
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		createEmptyResponseWithStatus(500, "")
+	}
+
+	if err = insertUserDataToCandleBackendUserTable(cfg, ctx, userData); err != nil {
+		createEmptyResponseWithStatus(500, "Data write error.")
+	}
+
+	jsonUserData, err := json.Marshal(userData)
+	if err != nil {
+		createEmptyResponseWithStatus(500, "JSON parse error.")
+	}
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("room/{%s}/%s", event.PathParameters["room_id"], event.HTTPMethod),
-		StatusCode: 200,
+		Body:       string(jsonUserData),
+		StatusCode: http.StatusOK,
 	}, nil
 }
 
+func createEmptyResponseWithStatus(statusCode int, responseMessage string) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       responseMessage,
+		StatusCode: statusCode,
+	}, nil
+}
+
+func insertUserDataToCandleBackendUserTable(cfg aws.Config, ctx context.Context, userData UserData) error {
+	svc := dynamodb.NewFromConfig(cfg)
+
+	tableName := "CandleBackendUserTable"
+	if t, exists := os.LookupEnv("USER_ABLE_NAME"); exists {
+		tableName = t
+	}
+
+	var dynamoValue []types.AttributeValue
+	for _, ans := range userData.Answers {
+		answerMap := map[string]types.AttributeValue{
+			"question_id": &types.AttributeValueMemberS{Value: ans.QuestionId},
+			"answer":      &types.AttributeValueMemberBOOL{Value: ans.Answer},
+		}
+		dynamoValue = append(dynamoValue, &types.AttributeValueMemberM{Value: answerMap})
+	}
+
+	_, err := svc.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]types.AttributeValue{
+			"user_id":  &types.AttributeValueMemberS{Value: userData.UserId},
+			"nickname": &types.AttributeValueMemberS{Value: userData.NickName},
+			"answers":  &types.AttributeValueMemberL{Value: dynamoValue},
+		}, ConditionExpression: aws.String("attribute_not_exists(nickname,answers)"),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
-	lambda.Start(handler)
+	lambda.Start(enterRoomHandler)
 }
