137a905b463ca44174a865588732f6109e6370b3
is-hoku <me@hokuishi.be>
2023-12-22 22:59:09 +0900

    set questions

--- lambda/questions/seed/main.go
+++ lambda/questions/seed/main.go
@@ -28,11 +28,26 @@ func InsertData(ctx context.Context, event cfn.Event) (string, map[string]interf
 	}
 
 	items := []QuestionItem{
-		{QuestionID: 1, Statement: "『ワンピース』は好きですか？"},
-		{QuestionID: 2, Statement: "『進撃の巨人』は好きですか？"},
-		{QuestionID: 3, Statement: "『ちいかわ』は好きですか？"},
+		{QuestionID: 1, Statement: "料理をすることは好きですか？"},
+		{QuestionID: 2, Statement: "読書は好きですか？"},
+		{QuestionID: 3, Statement: "映画鑑賞は好きですか？"},
 		{QuestionID: 4, Statement: "『ポケモン』は好きですか？"},
-		{QuestionID: 5, Statement: "温泉は好きですか？"},
+		{QuestionID: 5, Statement: "ジョギングは好きですか？"},
+		{QuestionID: 6, Statement: "カラオケは好きですか？"},
+		{QuestionID: 7, Statement: "コンサートに行くことは好きですか？"},
+		{QuestionID: 8, Statement: "ボードゲームは好きですか？"},
+		{QuestionID: 9, Statement: "アニメを見ることは好きですか？"},
+		{QuestionID: 10, Statement: "筋トレは好きですか？"},
+		{QuestionID: 11, Statement: "手芸や工作は好きですか？"},
+		{QuestionID: 12, Statement: "キャンプは好きですか？"},
+		{QuestionID: 13, Statement: "海外旅行は好きですか？"},
+		{QuestionID: 14, Statement: "幼少期にスポーツチームに所属していましたか？"},
+		{QuestionID: 15, Statement: "歴史の授業が好きでしたか？"},
+		{QuestionID: 16, Statement: "学校の科学の授業が得意でしたか？"},
+		{QuestionID: 17, Statement: "スパイシーな食べ物が好きですか？"},
+		{QuestionID: 18, Statement: "過去に100冊以上の本を読んだことがありますか？"},
+		{QuestionID: 19, Statement: "ペットを飼ったことがありますか？"},
+		{QuestionID: 20, Statement: "世の中の動向を追っていますか"},
 	}
 
 	for _, item := range items {
